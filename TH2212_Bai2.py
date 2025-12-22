abae41568d1f2b5f80f0e02e2291eedc5d39b419
NMPhat67khmt <phat.nm.67khmt@ntu.edu.vn>
2025-12-22 14:49:58 +0700

    Update TH2212_Bai2.py

--- TH2212_Bai2.py
+++ TH2212_Bai2.py
@@ -1,18 +1,14 @@
-so_lon_nhat=int(input("nhap so lon nhat"))
-so_nho_nhat=int(input("nhap so nho nhat"))
 a=int(input("nhap so a:"))
 b=int(input("nhap so b:"))
 c=int(input("nhap so c:"))
-if a > so_lon_nhat:
-    so_lon_nhat=a
-elif b > so_lon_nhat:
-       so_lon_nhat=b
+so_lon_nhat=a
+so_nho_nhat=a
+if b > so_lon_nhat:
+    so_lon_nhat=b
 elif c > so_lon_nhat:
-        so_lon_nhat=c
-if a < so_nho_nhat:
-    so_nho_nhat=a
-elif b < so_nho_nhat:
-        so_nho_nhat=b
+       so_lon_nhat=c
+if b < so_nho_nhat:
+    so_nho_nhat=b
 elif c < so_nho_nhat:
         so_nho_nhat=c
 print(f"So lon nhat la:",so_lon_nhat)
